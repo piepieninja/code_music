21539eb799e08119b4fc1aa68ba0226692ee3c84
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-04-01 00:13:05 -0400

    hello world and makefile

--- hello.asm
+++ hello.asm
@@ -1,16 +1,35 @@
-section	.text
-	global_start     	;must be declared for linker (ld)
+	;;  Hello World in assembly for mac
+	;;  nasm -f macho hello.asm
+	;;  ld -e _start -o hello hello.o
+	;;
+section     .text
+ global _start		;must be declared for linker (ld)
 
-_start:		            	;tells linker entry point
-	mov edx,len     	;message length
-	mov ecx,msg     	;message to write
-	mov ebx,1       	;file descriptor (stdout)
-	mov eax,4       	;system call number (sys_write)
-	int 0x80        	;call kernel
+_syscall:
+	int     0x80	;system call
+	ret
+
+_start:				;tell linker entry point
+
+	push    dword len	;message length
+	push    dword msg	;message to write
+	push    dword 1	;file descriptor (stdout)
+	mov     eax,0x4	;system call number (sys_write)
+	call    _syscall	;call kernel
+
+	;; the alternate way to call kernel:
+	;; push   eax
+	;; call   7:0
+
+	add     esp,12	;clean stack (3 arguments * 4)
 	
-	mov eax,1       	;system call number (sys_exit)
-	int 0x80        	;call kernel
+	push    dword 0	;exit code
+	mov     eax,0x1	;system call number (sys_exit)
+	call    _syscall	;call kernel
 
-	section	.data
-	msg db 'Hello, world!', 0xa ;our dear string
-	len equ $ - msg		    ;length of our dear string
+	;; we do not return from sys_exit,
+	;; there's no need to clean stack
+section .data
+
+msg     db      "Hello, world!",0xa ;our dear string
+len     equ     $ - msg		    ;length of our dear string
